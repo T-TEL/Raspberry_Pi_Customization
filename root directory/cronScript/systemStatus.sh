f7a808838db4f36902119c4f83ffb6a9cb544f02
Open Learning Exchange <ghana@ole.org>
2015-09-05 14:36:50 +0000

    Script Update
    Grouped Scripts and Logs in separate directories.

--- root directory/cronScript/systemStatus.sh
+++ root directory/cronScript/systemStatus.sh
@@ -4,8 +4,8 @@ timestamp() {
   date +"%Y-%m-%d %T"
 }
 read file_facilityID < /boot/facilityID.txt
-read prev_status < /root/prevPowerStatus.txt
-read prev_battery_level < /root/prevBatteryLevel.txt
+read prev_status < /root/holder/prevPowerStatus.txt
+read prev_battery_level < /root/holder/prevBatteryLevel.txt
 battery_level=$(/bin/upsc sollatek@localhost battery.charge)
 ups_status=$(/bin/upsc sollatek@localhost ups.status)
 
@@ -13,7 +13,7 @@ ups_status=$(/bin/upsc sollatek@localhost ups.status)
 {
 if [ "${battery_level}" != "${prev_battery_level}" ];
 then
- echo "${battery_level}" > "/root/prevBatteryLevel.txt"
+ echo "${battery_level}" > "/root/holder/prevBatteryLevel.txt"
  /usr/bin/php /root/save_logs/savelogIntodb.php "ups_battery_level" "${battery_level}" $file_facilityID
  echo "$(timestamp) BATTERY_STATUS ${battery_level}"
 fi
@@ -26,7 +26,7 @@ fi
 {
 if [ "${ups_status}" != "${prev_status}" ];
 then
- echo "${ups_status}" > "/root/prevPowerStatus.txt"
+ echo "${ups_status}" > "/root/holder/prevPowerStatus.txt"
  /usr/bin/php /root/save_logs/savelogIntodb.php "ups_power_status" "{$ups_status}" $file_facilityID
  echo "$(timestamp) UPS_STATUS ${ups_status}"
 fi
@@ -37,20 +37,20 @@ fi
 ###### Internet Status
 COUNT=4
 SERVERIP=197.253.18.93
-read prev_IntStatus < /root/prev_IntStatus.txt
+read prev_IntStatus < /root/holder/prev_IntStatus.txt
 /bin/ping -c 3 $SERVERIP > /dev/null 2>&1
 if [ $? -ne 0 ]
 then
 	if [ "Not Available" != "${prev_IntStatus}" ];
 	then
- 		echo "Not Available" > "/root/prev_IntStatus.txt"
+ 		echo "Not Available" > "/root/holder/prev_IntStatus.txt"
 	/usr/bin/php /root/save_logs/savelogIntodb.php "internet_status" "Not Available" $file_facilityID
  		echo "$(timestamp) Internet Not Available"
 	fi
 else
 	if [ "Available" != "${prev_IntStatus}" ];
         then
-                echo "Available" > "/root/prev_IntStatus.txt"
+                echo "Available" > "/root/holder/prev_IntStatus.txt"
          /usr/bin/php /root/save_logs/savelogIntodb.php "internet_status" "Available"  $file_facilityID
                 echo "$(timestamp) Internet Available"
         fi
@@ -59,10 +59,10 @@ fi
 ##### Network ISP
 {
 ISPsource="$(/usr/bin/php /root/getISP.php)"
-read prev_ISP < /root/prev_ISP.txt
+read prev_ISP < /root/holder/prev_ISP.txt
 if [ "${ISPsource}" != "${prev_ISP}" ];
 then
- echo "${ISPsource}" > "/root/prev_ISP.txt"
+ echo "${ISPsource}" > "/root/holder/prev_ISP.txt"
  /usr/bin/php /root/save_logs/savelogIntodb.php "internet_source" "${ISPsource}" $file_facilityID
  echo "$(timestamp) ISP ${ISPsource}"
 fi
