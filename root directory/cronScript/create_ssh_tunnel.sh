2baa759de069a4a1f93c864ce062a08160b274e1
Leonard Maximus Irenous Mensah <leonard.mensah@ole.org>
2015-09-07 07:15:37 +0000

    Update create_ssh_tunnel.sh

--- root directory/cronScript/create_ssh_tunnel.sh
+++ root directory/cronScript/create_ssh_tunnel.sh
@@ -4,7 +4,7 @@ createTunnel() {
   ###/usr/bin/ssh -N -R 2222:localhost:22 leomaxi@50.62.133.120
  /usr/bin/ssh -N -R $file_PortNumber:127.0.0.1:22 leomaxi@50.62.133.120 
 if [[ $? -eq 0 ]]; then
-    echo Tunnel to jumpbox created successfully
+    echo Tunnel to server created successfully
   else
     echo An error occurred creating a tunnel to jumpbox. RC was $?
   fi
